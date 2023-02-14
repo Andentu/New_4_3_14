f7cfd90975fb3c974113a080dcdb162bafea44a3
Andentu <severalmaz97@gmail.com>
2023-02-14 20:30:49 +0300

    Инициализирован зубчатый массив

--- New_4_3_14/Program.cs
+++ New_4_3_14/Program.cs
@@ -4,7 +4,11 @@
     {
         static void Main(string[] args)
         {
-            
+            int[][] array = new int[3][];
+            array[0] = new int[2] { 1, 2 };
+            array[1] = new int[3] { 1, 2, 3 };
+            array[2] = new int[5] { 1, 2, 3, 4, 5 };
+
         }
     }
 }
